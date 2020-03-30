4cf2d05c8f3dc5779a52ef1300049a6cefe4e0c2
Yoli <Yoli@DESKTOP-VVTJ7OQ>
2020-03-30 10:05:42 +0200

    Ejemplo de subida

---
+++ basico01/main.py
@@ -0,0 +1,19 @@
+'''
+Created on 9 mar. 2020
+
+@author: yoli
+'''
+#esto es un comentario, no se toma en cuenta cuando
+#se ejecute este codigo
+
+print("primera linea de codigo")
+print("segunda linea de codigo")
+#dos elementos imprescindibles en el mundo de la programacion es el
+#uso de variables y sentencias de control
+
+nombre = "yoli"
+#nombre = 12
+print("valor de la variable nombre: " + str(nombre))
+
+if nombre == "yoli":
+    print("hola yoli")
